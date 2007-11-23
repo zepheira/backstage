d2b4ee85172bd3e78b976d23471daa5cc61f53ce
David Huynh <dfhuynh@gmail.com>
2007-11-23 19:14:39 +0000

    Set modules.properties to serve ajax and exhibit APIs. Started to implement the ability to reconstruct the server's state from the client state; much is left to be done.
    git-svn-id: http://simile.mit.edu/repository/backstage/trunk@8833 983b779f-54db-0310-a2f8-e46c051b0de2

--- modules/backstage/api/scripts/backstage.js
+++ modules/backstage/api/scripts/backstage.js
@@ -21,7 +21,17 @@ Backstage._Impl.prototype.dispose = function() {
 };
 
 Backstage._Impl.prototype.asyncCall = function(method, params, onSuccess, onError) {
-    this._jsonpTransport.asyncCall(method, params, onSuccess, onError);
+    var self = this;
+    var f = function() {
+        self._jsonpTransport.asyncCall(method, params, onSuccess, function(e) {
+            if (e.code == 410) { // 410:Gone
+                self._reconstructServerState(f);
+            } else {
+                onError(e);
+            }
+        });
+    };
+    f();
 };
 
 Backstage._Impl.prototype.clearAsyncCalls = function() {
@@ -34,4 +44,22 @@ Backstage._Impl.prototype.queueJob = function(job) {
 
 Backstage._Impl.prototype.clearJobs = function() {
     this._jobQueue.clear();
+};
+
+Backstage._Impl.prototype._reconstructServerState = function(cont) {
+    var params = {}; // TODO: need to populate params
+    this._jsonpTransport.asyncCall(
+        "reconstruct", 
+        params, 
+        function(o) { 
+            // TODO: what to do with o?
+            cont();
+        }, 
+        function(e) {
+            /*
+             *  We couldn't reconstruct the server's state from the client's state. This is really bad.
+             */
+            alert("This session has been inactive for too long and it cannot be continued.\nPlease refresh the page to start a new session.");
+        }
+    );
 };
