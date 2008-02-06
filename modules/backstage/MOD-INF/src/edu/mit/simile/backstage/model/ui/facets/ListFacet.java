cdc6eee37c07a6dd0d5429e780a9b03ff4b8e821
David Huynh <dfhuynh@gmail.com>
2008-02-06 20:34:43 +0000

    Temporarily checked in ListFacet to debug on my other machine which has Sesame 2.0 rc2.
    git-svn-id: http://simile.mit.edu/repository/backstage/trunk@9099 983b779f-54db-0310-a2f8-e46c051b0de2

--- modules/backstage/MOD-INF/src/edu/mit/simile/backstage/model/ui/facets/ListFacet.java
+++ modules/backstage/MOD-INF/src/edu/mit/simile/backstage/model/ui/facets/ListFacet.java
@@ -1,24 +1,43 @@
 package edu.mit.simile.backstage.model.ui.facets;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.mozilla.javascript.Scriptable;
+import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 import org.openrdf.model.impl.LiteralImpl;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
 import org.openrdf.query.algebra.Compare;
+import org.openrdf.query.algebra.Count;
+import org.openrdf.query.algebra.Group;
+import org.openrdf.query.algebra.GroupElem;
 import org.openrdf.query.algebra.Or;
+import org.openrdf.query.algebra.Projection;
+import org.openrdf.query.algebra.ProjectionElem;
+import org.openrdf.query.algebra.ProjectionElemList;
 import org.openrdf.query.algebra.ValueExpr;
 import org.openrdf.query.algebra.Var;
 import org.openrdf.query.algebra.Compare.CompareOp;
+import org.openrdf.query.algebra.helpers.QueryModelTreePrinter;
+import org.openrdf.query.parser.ParsedTupleQuery;
+import org.openrdf.repository.sail.SailRepositoryConnection;
 
 import edu.mit.simile.backstage.model.BackChannel;
 import edu.mit.simile.backstage.model.Context;
 import edu.mit.simile.backstage.model.TupleQueryBuilder;
+import edu.mit.simile.backstage.model.data.Database;
 import edu.mit.simile.backstage.model.data.Expression;
 import edu.mit.simile.backstage.model.data.ExpressionException;
 import edu.mit.simile.backstage.model.data.ExpressionResult;
+import edu.mit.simile.backstage.util.DefaultScriptableObject;
+import edu.mit.simile.backstage.util.MyTupleQuery;
+import edu.mit.simile.backstage.util.ScriptableArrayBuilder;
 import edu.mit.simile.backstage.util.Utilities;
 
 public class ListFacet extends Facet {
@@ -30,6 +49,9 @@ public class ListFacet extends Facet {
     
     protected TupleQueryBuilder _builder;
     protected Var               _itemVar;
+    protected Var               _valueVar;
+    protected Var               _countVar;
+    protected String            _valueType;
     
     public ListFacet(Context context, String id) {
         super(context, id);
@@ -85,7 +107,72 @@ public class ListFacet extends Facet {
         _builder = queryBuilder;
         _itemVar = itemVar;
         
+        ExpressionResult result = _expression.computeOutputOnItem(
+            _context.getDatabase(), 
+            _builder, 
+            _itemVar
+        );
+        
+        if (!(result.valueExpr instanceof Var)) {
+            throw new ExpressionException("Facet expression does not evaluate to a Var");
+        }
+        
+        _valueVar = (Var) result.valueExpr;
+        _valueType = result.valueType;
+        _countVar = _builder.makeVar("count");
+        
         backChannel.addComponentChangingState(this);
+    }
+    
+    @Override
+    public Scriptable getComponentState() {
+        DefaultScriptableObject result = new DefaultScriptableObject();
+        ScriptableArrayBuilder facetChoices = new ScriptableArrayBuilder();
+        
+        Database database = _context.getDatabase();
+        try {
+            SailRepositoryConnection connection = (SailRepositoryConnection)
+                database.getRepository().getConnection();
+            
+            try {
+                Group group = new Group(_builder.makeFilterTupleExpr());
+                group.addGroupBindingName(_valueVar.getName());
+                group.addGroupBindingName(_itemVar.getName());
+                group.addGroupElement(new GroupElem(_countVar.getName(), new Count(_itemVar)));
+                
+                ProjectionElemList projectionElements = new ProjectionElemList();
+                projectionElements.addElement(new ProjectionElem(_valueVar.getName()));
+                projectionElements.addElement(new ProjectionElem(_countVar.getName()));
+                
+                Projection projection = new Projection(group, projectionElements);
+                
+                _logger.info(QueryModelTreePrinter.printTree(projection));
+                
+                TupleQuery query = new MyTupleQuery(new ParsedTupleQuery(projection), connection);
+                TupleQueryResult queryResult = query.evaluate();
+                try {
+                    while (queryResult.hasNext()) {
+                        BindingSet bindingSet = queryResult.next();
+                        
+                        for (String s : bindingSet.getBindingNames()) {
+                            _logger.info("   --" + s);
+                        }
+                        Value value = bindingSet.getValue(_valueVar.getName());
+                        Value count = bindingSet.getValue(_countVar.getName());
+                        
+                        _logger.info("--facet " + value + " " + count);
+                    }
+                } finally {
+                    queryResult.close();
+                }
+            } finally {
+                connection.close();
+            }
+        } catch (Exception e) {
+            _logger.error("Error querying to compute list facet", e);
+        }
+        
+        return result;
     }
     
     protected ValueExpr createRestrictionClause(
